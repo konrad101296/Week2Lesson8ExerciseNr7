5377f96a0c7d85d68725ce3531d3e3f1ed76f50f
konrad101296 <konradslimak@o2.pl>
2024-03-06 19:44:24 +0100

    Add project files.

---
+++ Week2Lesson8ExerciseNr7/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("podaj ile linijek ma miec diament: ");
+int lenght = int.Parse(Console.ReadLine());
+int divided = lenght / 2;
+int space = lenght / 2;
+int star = 1;
+for (int i = 0; i < lenght; i++)
+{
+    Console.WriteLine();
+    if (divided > i)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            Console.Write(" ");
+        }
+        space--;
+        for (int j = 0; j < star; j++)
+        {
+            Console.Write("*");
+        }
+        star += 2;
+    }
+    else if (divided <= i)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            Console.Write(" ");
+        }
+        space++;
+        for (int j = 0; j < star; j++)
+        {
+            Console.Write("*");
+        }
+        star -= 2;
+
+    }
+}
